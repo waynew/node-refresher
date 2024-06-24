c25d41d31ecc51d916d45641e2aecce72e5fd336
Wayne Werner <wayne@waynewerner.com>
2024-06-24 08:40:46 -0500

    Find out the shape of req
    console.log(req), what does it reveal? Well, there's a heck ton of info
    found. Looks like it's of IncomingMessage, if IIUC. Things of any
    interest to me:
    
    - .socket -- access to the underlying socket for any fun times; probably
      not often relevant
    - .server -- the current server instance?
    - .parser -- an http parser
    - .on -- probably useful for adding some onchange kind of tools
    - .httpVersion -- not *very* relevant, but there are certain http
      versions that might be problematic, from a security perspective
    - .rawHeaders -- what it says on the tin, raw HTTP headers
    - .url and .method -- in `HTTP 1.1 /somepath GET`, the `/somepath` and
      the `GET`. Useful for doing/deciding how to handle things.
    - addListener -- not sure exactly what signals we can listen to, but we
      can!
    - .accept -- the accept headers (often `*/*`, but could be something
      like `text/plain`!)

--- one.js
+++ one.js
@@ -4,6 +4,7 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = createServer((req, res) => {
+    console.log(req);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Heeeeey dude!');
